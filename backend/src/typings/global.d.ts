b296babab46ddb75d3f6d8d7bda21b35d5db8013
PeterP_Saxion <490498@student.saxion.nl>
2022-05-10 15:21:30 +0200

    feat: collaps database env configs into one

--- backend/src/typings/global.d.ts
+++ backend/src/typings/global.d.ts
@@ -1,11 +1,9 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            DATABASE_USR: string;
-            DATABASE_USR_PW: string;
-            DATABASE_NAME: string;
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      DATABASE_URL: string;
     }
+  }
 }
 
 export { };
