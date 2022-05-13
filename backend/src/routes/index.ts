b10e5ea36f263a978e64af1924e6625648906a7f
Quan <490848@student.saxion.nl>
2022-05-13 11:44:53 +0200

    merge: resolved conflicts from rebasing on PQ4-9

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,2 +1,3 @@
 export { default as objectsRouter } from './objects/objects.route';
+export { default as boardsRouter } from './boards.route';
 export { default as indexRouter } from './index/index.route';
