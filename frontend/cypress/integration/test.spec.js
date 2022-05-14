fde19bdf19778f7556d768028d5bc091ddb00be1
PeterP_Saxion <490498@student.saxion.nl>
2022-05-14 11:41:57 +0200

    test(cypress): add basic test for listing 2 items

--- frontend/cypress/integration/test.spec.js
+++ frontend/cypress/integration/test.spec.js
@@ -1,5 +1,16 @@
-describe('Initial test', () => {
-  it('Does not check anything', () => {
-    expect(true).to.equal(true);
+describe('View example data', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080');
+  });
+
+  describe('Click the "Read Data" button', () => {
+    it('Click Button', () => {
+      cy.get('#test-button')
+        .should('be.visible')
+        .click();
+
+      cy.get('#test-container')
+        .children().should('have.length', 2);
+    });
   });
 });
