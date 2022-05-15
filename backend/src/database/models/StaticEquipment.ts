06dd321a9af6eeee24c79c063f93040bd0011d1f
Quan <490848@student.saxion.nl>
2022-05-15 20:46:11 +0200

    feat: adding abstract attribute to @Entity classes

--- backend/src/database/models/StaticEquipment.ts
+++ backend/src/database/models/StaticEquipment.ts
@@ -1,7 +1,10 @@
 import { Entity } from '@mikro-orm/core';
 import MechanicalEquipment from './MechanicalEquipment';
 
-@Entity({ discriminatorValue: 'staticEquipment' })
+@Entity({ 
+    discriminatorValue: 'staticEquipment',
+    abstract: true
+})
 export default abstract class StaticEquipment extends MechanicalEquipment {
 
 }
