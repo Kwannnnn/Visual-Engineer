bae14cc75ec95a337e5d2e73e2165fd780bdcc2d
kristiyan2021 <449302@student.saxion.nl>
2022-05-15 12:54:01 +0200

    fix: issue with jest not closing ports properly

--- backend/src/routes/objects/objects.route.test.ts
+++ backend/src/routes/objects/objects.route.test.ts
@@ -1,41 +1,41 @@
 import request from 'supertest';
-import { Server } from 'http';
-import { server, setup } from '../../index';
+// import { Server } from 'http';
+import setup from '../../index';
 import DI from '../../DI';
 
-let httpServer: Server;
+// let httpServer: Server;
 let app: Express.Application;
 
 beforeEach(async () => {
-  app = await setup;
-  httpServer = server;
+  app = await setup();
 });
 
-afterEach((done) => {
-  DI.orm.close().then(() => {
-    httpServer.close(() => done());
+afterEach(() => {
+  DI.orm.close();
+});
+
+describe('/objects', () => {
+  describe('GET /objects', () => {
+    it('should return a list of all existing objects', async () => {
+      const response = await request(app).get('/api/v1/objects');
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual([]);
+    });
   });
-});
 
-describe('GET endpoints', () => {
-  // FIXME: Fix this test
-  // describe('GET api/v1/objects', () => {
-  //   test('should return all objects', async () => {
-  //     const response = await request(app).get('/api/v1/objects');
-  //     expect(response.status).toEqual(200);
-  //     expect(response.body).toEqual([]);
-  //   });
-  // });
-
-  describe('GET api/v1/objects/:tag', () => {
-    test('should return an existing object', async () => {
-      // TODO: update this test whenever database is seeded
-      expect(true);
+  describe('GET /objects/:tag', () => {
+    describe('given the object does exist', () => {
+      it('should return an existing object', async () => {
+        // TODO: update this test whenever database is seeded
+        expect(true).toBe(true);
+      });
     });
 
-    test('should return 404 when object with tag does not exist', async () => {
-      const response = await request(app).get('/api/v1/objects/3737');
-      expect(response.status).toEqual(404);
+    describe('given the object does not exist', () => {
+      it('should return 404', async () => {
+        const response = await request(app).get('/api/v1/objects/3737');
+        expect(response.status).toEqual(404);
+      });
     });
   });
 });
