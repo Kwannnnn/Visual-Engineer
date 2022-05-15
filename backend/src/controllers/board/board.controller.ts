8664e9e83b1e4226b21621beb7384ca3795d566d
Quan <490848@student.saxion.nl>
2022-05-15 20:54:55 +0200

    merge: resolved merge conflicts from rebasing on staging

--- backend/src/controllers/board/board.controller.ts
+++ backend/src/controllers/board/board.controller.ts
@@ -4,28 +4,28 @@ import { Item, Board, Pipeline } from '../../database/models';
 import PressureClass from '../../database/models/PressureClass.enum';
 
 export const getAll = async (req: Request, res: Response) => {
-    const boards = await DI.boardRepository.findAll();
-    res.send(boards);
+  const boards = await DI.boardRepository.findAll();
+  res.send(boards);
 };
 
 export const getById = async (req: Request, res: Response) => {
-    const id: number = +req.params.id;
+  const id: number = +req.params.id;
 
-    try {
-        const board = await DI.boardRepository.findOne(id);
+  try {
+    const board = await DI.boardRepository.findOne(id);
 
-        if (!board) {
-            return res.status(404).json({
-                message: 'Board not found',
-            });
-        }
-
-        return res.json(board);
-    } catch (e: any) {
-        return res.status(400).json({
-            message: e.message,
-        });
+    if (!board) {
+      return res.status(404).json({
+        message: 'Board not found',
+      });
     }
+
+    return res.json(board);
+  } catch (e: any) {
+    return res.status(400).json({
+      message: e.message,
+    });
+  }
 };
 
 export const getBoardObjects = async (req: Request, res: Response) => {
@@ -58,20 +58,20 @@ export const postBoard = async (req: Request, res: Response) => {
         return res.json({ message: 'Board name is missing' });
     }
 
-    try {
-        const board = DI.em.create(Board, req.body);
-        await DI.boardRepository.persist(board).flush();
+  try {
+    const board = DI.em.create(Board, req.body);
+    await DI.boardRepository.persist(board).flush();
 
-        return res.status(201).json(board);
-    } catch (e: any) {
-        return res.status(400).json({
-            message: e.message,
-        });
-    }
+    return res.status(201).json(board);
+  } catch (e: any) {
+    return res.status(400).json({
+      message: e.message,
+    });
+  }
 };
 
 export const postItemToBoard = async (req: Request, res: Response) => {
-    if (!req.body.tag
+  if (!req.body.tag
         || !req.body.name
         || !req.body.length
         || !req.body.width
@@ -82,15 +82,15 @@ export const postItemToBoard = async (req: Request, res: Response) => {
         return res.json({ message: 'Item properties are missing' });
     }
 
-    try {
-        const id: number = +req.params.id;
-        const board = await DI.boardRepository.findOne(id);
+  try {
+    const id: number = +req.params.id;
+    const board = await DI.boardRepository.findOne(id);
 
-        if (!board) {
-            return res.status(404).json({
-                message: 'Board not found',
-            });
-        }
+    if (!board) {
+      return res.status(404).json({
+        message: 'Board not found',
+      });
+    }
 
     const pipeline = new Pipeline(
         '132-45a',
