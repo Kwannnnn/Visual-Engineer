4f36894ffaea0063063b2a297c58ca6b7d022232
Quan <490848@student.saxion.nl>
2022-05-17 13:24:09 +0200

    feat: removing 'abstract' attribute in @Entity annotations

--- backend/src/database/models/StaticEquipment.ts
+++ backend/src/database/models/StaticEquipment.ts
@@ -3,7 +3,6 @@ import MechanicalEquipment from './MechanicalEquipment';
 
 @Entity({
   discriminatorValue: 'staticEquipment',
-  abstract: true,
 })
 export default abstract class StaticEquipment extends MechanicalEquipment {
 
