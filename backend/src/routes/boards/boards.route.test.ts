0127892ec5f82cb7df536b8966e19b07685cdd7b
Quan <490848@student.saxion.nl>
2022-05-18 01:55:43 +0200

    feat: tests for post board endpoints completed

--- backend/src/routes/boards/boards.route.test.ts
+++ backend/src/routes/boards/boards.route.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import setup from '../../index';
 import DI from '../../DI';
+import { v4 as uuidv4 } from 'uuid';
+
 
 let app: Express.Application;
 
@@ -9,8 +11,29 @@ beforeEach(async () => {
 });
 
 afterEach(() => {
+  exampleItem.tag = uuidv4(); // generate unique tag for item after each test
   DI.orm.close();
 });
+
+let exampleItem = {
+  tag: '#583FA293D3',
+  name: 'Cleaner',
+  length: 2.52,
+  width: 2.35,
+  depth: 1.47,
+  diameter: 1.79,
+  emptyMass: 69,
+  head: 1,
+  filledMass: 5.4,
+  netVolume: 12.3,
+  grossVolume: 23.7,
+  preliminaryPower: 454,
+  finalPower: 600,
+  type: "pump"
+};
+
+//TODO: Drop and then update the database before running the test
+//TODO: since the database is not seeded yet
 
 describe('GET Board endpoints', () => {
   describe('GET api/v1/boards', () => {
@@ -32,6 +55,14 @@ describe('GET Board endpoints', () => {
       expect(response.statusCode).toEqual(404);
     });
   });
+
+  describe('GET api/v1/boards/:tag/objects', () => {
+    test('should return an existing board', async () => {
+      // TODO: update this test whenever database is seeded
+      // const response = await request(app).get('/api/v1/boards/1/objects');
+      expect(true);
+    });
+  });
 });
 
 describe('POST Board endpoints', () => {
@@ -44,6 +75,13 @@ describe('POST Board endpoints', () => {
         });
 
       expect(response.statusCode).toEqual(201);
+      expect(response.body).toEqual(
+        {
+          id: 1,
+          items: [],
+          name: 'new board',
+        }
+      );
     });
 
     test('should return 400 when board name is missing', async () => {
@@ -55,45 +93,51 @@ describe('POST Board endpoints', () => {
   });
 
   // TODO: the tests below are not finished
-  describe('POST api/v1/boards/:id/items', () => {
-    test('should return the posted board', async () => {
-      // first post a board
-      // const firstResponse = await request(app)
-      //   .post('/api/v1/boards')
-      //   .send({
-      //     name: 'new board',
-      //   });
+  describe('POST api/v1/boards/:id/objects', () => {
+    test('should return the posted object', async () => {
+      const response = await request(app)
+        .post(`/api/v1/boards/1/objects`)
+        .send(exampleItem);
 
-      // get id of the new board
-      // const { id } = firstResponse.body;
-
-      // const response = await request(app)
-      //   .post(`/api/v1/boards/${id}/items`)
-      //   .send({
-
-      //   });
-
-      expect(true);
+      expect(response.statusCode).toEqual(201);
+      expect(response.body).toEqual({
+        ...exampleItem,
+        "board": {
+          "id": 1,
+          "name": "new board"
+      }
+      });
     });
 
     test('should return 404 when board with id does not exist', async () => {
-      // const response = await request(app)
-      //   .post('/api/v1/boards/6969/items')
-      //   .send({
+      const response = await request(app)
+        .post('/api/v1/boards/6969/objects')
+        .send(exampleItem);
 
-      //   });
-
-      expect(true);
+      expect(response.statusCode).toEqual(404);
     });
 
-    test('should return 400 when one or more item attributes are missing', async () => {
-      // const response = await request(app)
-      //   .post('/api/v1/boards/1/items')
-      //   .send({
+    test('should return 400 when one or more object attributes are missing', async () => {
+      const response = await request(app)
+        .post('/api/v1/boards/1/objects')
+        .send({
+          tag: '#583FA293D3',
+          name: 'Cleaner',
+          // length: 2.52,
+          // width: 2.35,
+          depth: 1.47,
+          diameter: 1.79,
+          emptyMass: 69,
+          head: 1,
+          filledMass: 5.4,
+          netVolume: 12.3,
+          grossVolume: 23.7,
+          preliminaryPower: 454,
+          finalPower: 600,
+          type: "pump"
+        });
 
-      //   });
-
-      expect(true);
+      expect(response.statusCode).toEqual(400);
     });
   });
 });
