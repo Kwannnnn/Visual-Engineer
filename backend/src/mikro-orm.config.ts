ed996825c27045f84397c45d28f61e006673dacc
kristiyan2021 <449302@student.saxion.nl>
2022-05-18 09:30:20 +0200

    feat(backend): configurate mikro-orm seeder

--- backend/src/mikro-orm.config.ts
+++ backend/src/mikro-orm.config.ts
@@ -6,4 +6,12 @@ export default {
   clientUrl: process.env.DATABASE_URL,
   type: 'postgresql',
   metadataProvider: ReflectMetadataProvider,
+  seeder: {
+    path: './dist/database/seeders',
+    pathTs: './src/database/seeders',
+    defaultSeeder: 'DatabaseSeeder',
+    glob: '!(*.d).{js,ts}',
+    emit: 'ts',
+    fileName: (className: string) => className,
+  },
 };
