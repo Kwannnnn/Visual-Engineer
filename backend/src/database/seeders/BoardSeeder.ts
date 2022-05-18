9676825c89d965055a7b51a922e8c71e1cd2b907
kristiyan2021 <449302@student.saxion.nl>
2022-05-18 20:43:07 +0200

    feat(backend): update seeders to create data dynamically

--- backend/src/database/seeders/BoardSeeder.ts
+++ backend/src/database/seeders/BoardSeeder.ts
@@ -2,14 +2,26 @@
 import type { Dictionary, EntityManager } from '@mikro-orm/core';
 import { Seeder } from '@mikro-orm/seeder';
 import BoardFactory from '../factories/BoardFactory';
+import Board from '../models/Board';
 
+export const sampleBoards: Board[] = [];
 export default class BoardSeeder extends Seeder {
   async run(em: EntityManager, context: Dictionary): Promise<void> {
-    const boardFactory = new BoardFactory(em);
-    context.board = boardFactory.makeOne({
-      name: 'testBoard',
-    });
+    // Make sure to empty the array every time the seeder is used
+    sampleBoards.length = 0;
 
-    boardFactory.make(2);
+    // Create 3 sample boards
+    const boards = new BoardFactory(em).make(3);
+
+    /*
+    * Attach the newly created boards to a shared context, so that
+    * they can be referenced by other seeders.
+    */
+    context.sampleBoards = boards;
+
+    // Assign the boards to the exported array
+    boards.forEach((board) => {
+      sampleBoards.push(board);
+    });
   }
 }
--- backend/src/database/seeders/ObjectSeeder.ts
+++ backend/src/database/seeders/ObjectSeeder.ts
@@ -1,23 +1,24 @@
 /* eslint-disable class-methods-use-this */
 import type { Dictionary, EntityManager } from '@mikro-orm/core';
 import { Seeder } from '@mikro-orm/seeder';
-import { Pipeline } from '../models';
-import PressureClass from '../models/PressureClass.enum';
+import { BlowerFactory, PipelineFactory, PumpFactory } from '../factories';
+import { Item } from '../models';
 
+export const sampleBoardObjects: Item[] = [];
 export default class ObjectSeeder extends Seeder {
   async run(em: EntityManager, context: Dictionary): Promise<void> {
-    const item = new Pipeline(
-      '69',
-      'seedPipeline',
-      69.69,
-      69.69,
-      69.69,
-      69.69,
-      PressureClass.PN10,
-      'seedFlange',
-      'seedLining',
-    );
-    em.create(Pipeline, item);
-    context.board.items.add(item);
+    // Make sure to empty the array every time the seeder is used
+    sampleBoardObjects.length = 0;
+
+    // Get the board items list form the board attached to context
+    const { id: sampleBoardId, items: sampleBoardItems } = await context.sampleBoards[0];
+
+    // Add 3 sample items to the exported array
+    sampleBoardObjects.push(new PipelineFactory(em).makeOne({ board: sampleBoardId }));
+    sampleBoardObjects.push(new PumpFactory(em).makeOne({ board: sampleBoardId }));
+    sampleBoardObjects.push(new BlowerFactory(em).makeOne({ board: sampleBoardId }));
+
+    // Assign the newly created items to the sample board
+    sampleBoardObjects.forEach((object) => sampleBoardItems.add(object));
   }
 }
