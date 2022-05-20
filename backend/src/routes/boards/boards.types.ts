dbdcf04a450da3c640ad4d3fa85c30c0002d2b59
Philippos Slicher <philipposslicher@gmail.com>
2022-05-20 18:33:37 +0200

    chore: remove commented out code

--- backend/src/routes/boards/boards.types.ts
+++ backend/src/routes/boards/boards.types.ts
@@ -11,11 +11,6 @@ export type PatchBoardBody = Pick<Board, 'name'>;
 export interface BoardObjectParams extends BoardParams {
   objectId: string;
 }
-
-// export type PatchBoardObject = Omit<
-// PipeFitting | Pipeline | Pump | Blower | Tank | Vessel,
-// 'tag' | 'board'>;
-
 interface IPatchBoardObject extends Pipeline, PipeFitting, Pump, Blower, Tank, Vessel {
 
 }
