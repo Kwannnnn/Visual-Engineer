a1df89c0751fa9bc804f296dcaab4cece7525a56
Melissa Neamt <498265@student.saxion.nl>
2022-05-21 19:46:08 +0200

    feat: add dnd functionality to ToolboxItem component

--- frontend/src/components/ToolboxItem.tsx
+++ frontend/src/components/ToolboxItem.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCube } from '@fortawesome/free-solid-svg-icons';
+import { useDrag } from 'react-dnd';
 
 interface Item {
-    name: string;
-    className?: string;
-  }
+  className?: string;
+  name: string;
+}
 
 function ToolboxItem({ name, className = '' }: Item) {
+  const [, drag] = useDrag({
+    type: 'item',
+    item: { name },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
   return (
-    <div className={`cursor-move select-none ${className}`}>
+    <div ref={drag} className={`cursor-move select-none ${className}`}>
       <FontAwesomeIcon icon={faCube} />
       {` ${name}`}
     </div>
