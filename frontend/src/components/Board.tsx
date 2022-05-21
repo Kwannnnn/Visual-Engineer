be6ff29469a706034b1545095cb1e57dd47d1492
Melissa Neamt <498265@student.saxion.nl>
2022-05-21 19:44:51 +0200

    feat: add items list to the Board component

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,28 +1,50 @@
 import React, { useState } from 'react';
 import { useDrop } from 'react-dnd';
-import Item from './Item';
-import { Items } from './ItemsList';
+import BoardItem from './BoardItem';
 
 interface BoardProps {
   className?: string;
 }
 
+const items = [
+  {
+    tag: 'PPP-1234',
+    name: 'Plastic pipe',
+  },
+  {
+    tag: 'PIP-2345',
+    name: 'Iron pipe',
+  },
+  {
+    tag: 'SCP-3456',
+    name: 'Copper square pipe',
+  },
+  {
+    tag: 'SAP-4567',
+    name: 'Aluminum square pipe',
+  },
+  {
+    tag: 'CAC-5678',
+    name: '2A cleaner',
+  },
+  {
+    tag: 'CHC-6789',
+    name: 'F3 hot cleaner',
+  }
+];
+
 function Board({ className }: BoardProps) {
   const [board, setBoard] = useState<{ tag: string; name: string }[]>([]);
 
   const addItemToBoard = (itemName: string) => {
-    const item = Items.find((i) => i.name === itemName);
-
-    if (item === undefined) {
-      return;
-    }
+    const item = items.find((i) => i.name === itemName)!;
 
     setBoard((boardArray) => [...boardArray, item]);
   };
 
   const [, drop] = useDrop(() => ({
     accept: 'item',
-    drop: (item: typeof Item) => addItemToBoard(item.name),
+    drop: (item: { tag: string; name: string }) => addItemToBoard(item.name),
     collect: (monitor) => ({
       isOver: monitor.isOver(),
     }),
@@ -31,12 +53,12 @@ function Board({ className }: BoardProps) {
   return (
     <main ref={drop} className={`overflow-auto ${className}`}>
       <div className="flex flex-col">
-        {board.map((item) => (
-          <Item
+        {board?.map((item) => (
+          <BoardItem
             key={item.tag}
             tag={item.tag}
             name={item.name}
-            className="px-3 bg-gray-50 rounded w-24 h-16 flex flex-col justify-center items-center"
+            className="px-3 bg-gray-50 w-32 h-24 flex flex-col justify-center items-center"
           />
         ))}
       </div>
