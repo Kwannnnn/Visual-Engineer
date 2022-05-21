66d60aaf662deccefbd02fa84c4a191ae79ae15e
PeterP_Saxion <490498@student.saxion.nl>
2022-05-21 12:53:14 +0200

    feat: apply review suggestions

--- frontend/src/components/ToolboxItem.tsx
+++ frontend/src/components/ToolboxItem.tsx
@@ -4,11 +4,12 @@ import { faCube } from '@fortawesome/free-solid-svg-icons';
 
 interface Item {
     name: string;
+    className?: string;
   }
 
-function ToolboxItem({ name }: Item) {
+function ToolboxItem({ name, className = '' }: Item) {
   return (
-    <div className="cursor-move select-none">
+    <div className={`cursor-move select-none ${className}`}>
       <FontAwesomeIcon icon={faCube} />
       {` ${name}`}
     </div>
