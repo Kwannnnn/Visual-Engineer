a165cb8b348d40e950b8b913a47bd9aaa274cd74
Melissa Neamt <498265@student.saxion.nl>
2022-05-21 12:31:00 +0200

    feat: add dnd functionality to the Board component

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDrop } from 'react-dnd';
+import Item from './Item';
+import { Items } from './ItemsList';
 
 interface BoardProps {
-  className?: string
+  className?: string;
 }
 
 function Board({ className }: BoardProps) {
+  const [board, setBoard] = useState<{ tag: string; name: string }[]>([]);
+
+  const addItemToBoard = (itemName: string) => {
+    const item = Items.find((i) => i.name === itemName);
+
+    if (item === undefined) {
+      return;
+    }
+
+    setBoard((boardArray) => [...boardArray, item]);
+  };
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: 'item',
+    drop: (item: typeof Item) => addItemToBoard(item.name),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+    }),
+  }));
+
   return (
-    <main className={`overflow-auto ${className}`}>
-      Board
+    <main ref={drop} className={`overflow-auto ${className}`}>
+      <div className="flex flex-col">
+        {board.map((item) => (
+          <Item
+            key={item.tag}
+            tag={item.tag}
+            name={item.name}
+            className="px-3 bg-gray-50 rounded w-24 h-16 flex flex-col justify-center items-center"
+          />
+        ))}
+      </div>
     </main>
   );
 }
