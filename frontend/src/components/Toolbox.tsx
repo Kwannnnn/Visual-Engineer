3924d062aacf652a0b93db963031d26675dd3025
Melissa Neamt <498265@student.saxion.nl>
2022-05-21 12:30:21 +0200

    feat: display items in the Toolbox sidebar, remove Lorem ipsum

--- frontend/src/components/Toolbox.tsx
+++ frontend/src/components/Toolbox.tsx
@@ -1,80 +1,14 @@
 import React from 'react';
+import ItemsList from './ItemsList';
 
 interface ToolboxProps {
-  className?: string
+  className?: string;
 }
 
 function Toolbox({ className }: ToolboxProps) {
   return (
     <aside className={`bg-yellow-500 overflow-y-auto ${className}`}>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget ex accumsan,
-        aliquet
-        quam vitae, pharetra lorem. Cras viverra dui non sem mattis fringilla. Vestibulum leo
-        libero,
-        molestie et bibendum vel, molestie non enim. Donec finibus elit facilisis metus iaculis,
-        eget
-        dapibus neque rhoncus. In risus risus, convallis id sapien eget, cursus convallis sapien. Ut
-        sit amet lacinia quam, nec molestie justo. Etiam feugiat varius convallis.
-
-        Aliquam fermentum malesuada molestie. Sed erat nisl, placerat sit amet consectetur at,
-        rutrum
-        sed augue. Praesent nec mi bibendum, sagittis massa id, luctus eros. Vestibulum ante ipsum
-        primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras elementum dignissim
-        elit quis eleifend. Praesent pellentesque diam blandit dolor consectetur eleifend. Etiam ac
-        facilisis sem, a eleifend nulla. Phasellus fringilla porta venenatis. Phasellus a tellus
-        nisi.
-        Pellentesque mi orci, blandit eu tristique et, malesuada rutrum justo. Integer a ex dapibus,
-        auctor libero sollicitudin, pretium turpis.
-
-        Suspendisse sed feugiat odio, ut finibus erat. Aenean quis malesuada dolor. Quisque at
-        vestibulum diam. Mauris faucibus aliquet augue, non vehicula orci pretium sit amet. Sed
-        imperdiet ex felis, ac scelerisque diam gravida efficitur. In hac habitasse platea dictumst.
-        Fusce eget purus quis turpis congue hendrerit. Maecenas sapien erat, placerat
-        id vehicula qui
-        rutrum vitae sem. Sed malesuada, dui dignissim viverra ultrices, erat quam consequat arcu,
-        sit amet ultrices urna augue vel ipsum. Aliquam auctor eros dolor, id commodo turpis aliquet
-        vel. Phasellus ornare molestie lorem sed finibus. Quisque auctor nibh quis dui aliquam
-        feugiat.
-
-        Suspendisse sed feugiat odio, ut finibus erat. Aenean quis malesuada dolor. Quisque at
-        vestibulum diam. Mauris faucibus aliquet augue, non vehicula orci pretium sit amet. Sed
-        imperdiet ex felis, ac scelerisque diam gravida efficitur. In hac habitasse platea dictumst.
-        Fusce eget purus quis turpis congue hendrerit. Maecenas sapien erat, placerat
-        id vehicula qui
-        rutrum vitae sem. Sed malesuada, dui dignissim viverra ultrices, erat quam consequat arcu,
-        sit amet ultrices urna augue vel ipsum. Aliquam auctor eros dolor, id commodo turpis aliquet
-        vel. Phasellus ornare molestie lorem sed finibus. Quisque auctor nibh quis dui aliquam
-        feugiat.
-        Suspendisse sed feugiat odio, ut finibus erat. Aenean quis malesuada dolor. Quisque at
-        vestibulum diam. Mauris faucibus aliquet augue, non vehicula orci pretium sit amet. Sed
-        imperdiet ex felis, ac scelerisque diam gravida efficitur. In hac habitasse platea dictumst.
-        Fusce eget purus quis turpis congue hendrerit. Maecenas sapien erat, placerat
-        id vehicula qui
-        rutrum vitae sem. Sed malesuada, dui dignissim viverra ultrices, erat quam consequat arcu,
-        sit amet ultrices urna augue vel ipsum. Aliquam auctor eros dolor, id commodo turpis aliquet
-        vel. Phasellus ornare molestie lorem sed finibus. Quisque auctor nibh quis dui aliquam
-        feugiat.
-        Suspendisse sed feugiat odio, ut finibus erat. Aenean quis malesuada dolor. Quisque at
-        vestibulum diam. Mauris faucibus aliquet augue, non vehicula orci pretium sit amet. Sed
-        imperdiet ex felis, ac scelerisque diam gravida efficitur. In hac habitasse platea dictumst.
-        Fusce eget purus quis turpis congue hendrerit. Maecenas sapien erat, placerat
-        id vehicula qui
-        rutrum vitae sem. Sed malesuada, dui dignissim viverra ultrices, erat quam consequat arcu,
-        sit amet ultrices urna augue vel ipsum. Aliquam auctor eros dolor, id commodo turpis
-        vel. Phasellus ornare molestie lorem sed finibus. Quisque auctor nibh quis dui aliquam
-        feugiat.
-        Suspendisse sed feugiat odio, ut finibus erat. Aenean quis malesuada dolor. Quisque at
-        vestibulum diam. Mauris faucibus aliquet augue, non vehicula orci pretium sit amet. Sed
-        imperdiet ex felis, ac scelerisque diam gravida efficitur. In hac habitasse platea dictumst.
-        Fusce eget purus quis turpis congue hendrerit. Maecenas sapien erat, placerat
-        id vehicula qui
-        rutrum vitae sem. Sed malesuada, dui dignissim viverra ultrices, erat quam consequat arcu,
-        sit amet ultrices urna augue vel ipsum. Aliquam auctor eros dolor, id commodo turpis aliquet
-        vel. Phasellus ornare molestie lorem sed finibus. Quisque auctor nibh quis dui aliquam
-        feugiat.
-        feugiat.
-      </p>
+      <ItemsList />
     </aside>
   );
 }
