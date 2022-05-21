b851c6ac85859ddff3884a75ddd2d1df7c943967
Melissa Neamt <498265@student.saxion.nl>
2022-05-21 12:25:28 +0200

    feat: add DnDProvider to Home component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import Board from '../components/Board';
 import PropertiesSidebar from '../components/PropertiesSidebar';
 import Toolbox from '../components/Toolbox';
 
 function Home() {
   return (
-    <div className="flex-1 min-h-0">
-      <div className="grid grid-cols-12 h-full">
-        <Toolbox className="col-span-2" />
-        <Board className="col-span-7" />
-        <PropertiesSidebar className="col-span-3" />
+    <DndProvider backend={HTML5Backend}>
+      <div className="flex-1 min-h-0">
+        <div className="grid grid-cols-12 h-full">
+          <Toolbox className="col-span-2" />
+          <Board className="col-span-7" />
+          <PropertiesSidebar className="col-span-3" />
+        </div>
       </div>
-    </div>
+    </DndProvider>
   );
 }
 
