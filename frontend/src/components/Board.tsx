f32a4011e506b12b64989fc21e9d9a55eb2530d6
Melissa Neamt <498265@student.saxion.nl>
2022-05-22 15:02:10 +0200

    feat(board): calculate item's position

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDrop } from 'react-dnd';
+import update from 'immutability-helper';
 import BoardItem from './BoardItem';
 import DropPlaceholder from './DropPlaceholder';
 
@@ -11,50 +12,78 @@ const items = [
   {
     tag: 'PPP-1234',
     name: 'Plastic pipe',
+    left: 0,
+    top: 0,
   },
   {
     tag: 'PIP-2345',
     name: 'Iron pipe',
+    left: 0,
+    top: 0,
   },
   {
     tag: 'SCP-3456',
     name: 'Copper square pipe',
+    left: 0,
+    top: 0,
   },
   {
     tag: 'SAP-4567',
     name: 'Aluminum square pipe',
+    left: 0,
+    top: 0,
   },
   {
     tag: 'CAC-5678',
     name: '2A cleaner',
+    left: 0,
+    top: 0,
   },
   {
     tag: 'CHC-6789',
     name: 'F3 hot cleaner',
+    left: 0,
+    top: 0,
   }
 ];
 
 function Board({ className }: BoardProps) {
-  const [board, setBoard] = useState<{ tag: string; name: string }[]>([]);
+  const [board, setBoard] = useState<
+    { tag: string; name: string; left: number; top: number }[]
+  >([]);
 
-  const addItemToBoard = (itemName: string) => {
-    const item = items.find((i) => i.name === itemName);
+  const [{ canDrop }, drop] = useDrop(
+    () => ({
+      accept: 'item',
+      canDrop: () => true,
+      collect: (monitor) => ({
+        canDrop: monitor.canDrop(),
+        isOver: monitor.isOver(),
+      }),
+      drop(item: any, monitor) {
+        const foundItem = items.find((i) => i.name === item.name);
 
-    if (item === undefined) {
-      return;
-    }
+        if (foundItem === undefined) {
+          return;
+        }
 
-    setBoard((boardArray) => [...boardArray, item]);
-  };
+        const delta = monitor.getInitialClientOffset();
 
-  const [{ canDrop }, drop] = useDrop(() => ({
-    accept: 'item',
-    drop: (item: { tag: string; name: string }) => addItemToBoard(item.name),
-    collect: (monitor) => ({
-      canDrop: monitor.canDrop(),
-      isOver: monitor.isOver(),
+        if (delta) {
+          const roundedLeft = Math.round(foundItem.left + delta.x);
+          const roundedTop = Math.round(foundItem.top + delta.y);
+
+          const newItem = update(foundItem, {
+            left: { $set: roundedLeft },
+            top: { $set: roundedTop },
+          });
+
+          setBoard((boardArray) => [...boardArray, newItem]);
+        }
+      },
     }),
-  }));
+    [board, setBoard]
+  );
 
   return (
     <main className={`overflow-auto ${className}`}>
