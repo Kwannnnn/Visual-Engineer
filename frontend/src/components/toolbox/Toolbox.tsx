8243a0d4450d806e15363ff809bb1fab3a5a4440
Quan <490848@student.saxion.nl>
2022-05-31 22:28:11 +0200

    feat(frontend): fetch types and render toolbox

--- frontend/src/components/toolbox/Toolbox.tsx
+++ frontend/src/components/toolbox/Toolbox.tsx
@@ -1,48 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getObjectTypes } from '../../api/utility-functions';
 import ToolboxList from './ToolboxList';
 
 interface ToolboxProps {
   className?: string
 }
 
-const apiSampleData = [
-  {
-    group: 'Item',
-    subsets: [
-      {
-        group: 'Pipe Item',
-        items: [
-          { name: 'Pipe Fitting' }
-        ],
-      },
-      {
-        group: 'Mechanical Equipment',
-        subsets: [
-          {
-            group: 'Rotating Equipment',
-            items: [
-              { name: 'Pump' },
-              { name: 'Blower' }
-            ],
-          },
-          {
-            group: 'Static Equipment',
-            items: [
-              { name: 'Tank' },
-              { name: 'Vessel' }
-            ],
-          }
-        ],
-      }
-    ],
-  }
-];
+// const apiSampleData = [
+//   {
+//     group: 'Item',
+//     subsets: [
+//       {
+//         group: 'Pipe Item',
+//         items: [
+//           { name: 'Pipe Fitting' }
+//         ],
+//       },
+//       {
+//         group: 'Mechanical Equipment',
+//         subsets: [
+//           {
+//             group: 'Rotating Equipment',
+//             items: [
+//               { name: 'Pump' },
+//               { name: 'Blower' }
+//             ],
+//           },
+//           {
+//             group: 'Static Equipment',
+//             items: [
+//               { name: 'Tank' },
+//               { name: 'Vessel' }
+//             ],
+//           }
+//         ],
+//       }
+//     ],
+//   }
+// ];
 
 function Toolbox({ className }: ToolboxProps) {
+  const [types, setTypes] = useState([]);
+
+  useEffect(() => {
+    getObjectTypes()
+      .then((res) => {
+        setTypes(res);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  });
+
   return (
     <aside className={`overflow-y-auto ${className}`}>
       <div id="toolbox-list" className="p-3">
-        <ToolboxList listing={apiSampleData} subsetNbr={1} />
+        <ToolboxList listing={types} subsetNbr={1} />
       </div>
     </aside>
   );
