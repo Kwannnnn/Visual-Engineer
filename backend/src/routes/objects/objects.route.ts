ec68cb2aa404247d81764b3d60ac69107774403d
Quan <490848@student.saxion.nl>
2022-05-31 22:19:50 +0200

    docs: apidoc for object types route

--- backend/src/routes/objects/objects.route.ts
+++ backend/src/routes/objects/objects.route.ts
@@ -35,6 +35,51 @@ const objectsRouter: Router = Router();
 objectsRouter.get('/', objectController.getAll);
 
 /**
+ * @api {get} /api/v1/objects/types Get all object types
+ * @apiDescription Returns a resource response containing all object types in the system and their
+ *                 hierachy.
+ * @apiVersion 1.0.0
+ * @apiName GetObjectTypes
+ * @apiGroup Object
+ *
+ * @apiSuccess (Success 200) {Object[]} types JSON objects with object types arranged in a
+ *                                            hierachical manner
+ * @apiSuccessExample Success-Response:
+ * HTTP/1.1 200 OK
+ *     [{
+ *       "group": "Item",
+ *       "subsets": [
+ *         {
+ *           "group": "Pipe Item",
+ *           "items": [
+ *             { "name": "Pipe Fitting" }
+ *           ],
+ *         },
+ *         {
+ *           "group": "Mechanical Equipment",
+ *           "subsets": [
+ *             {
+ *               "group": "Rotating Equipment",
+ *               "items": [
+ *                 { "name": "Pump" },
+ *                 { "name": "Blower" }
+ *               ],
+ *             },
+ *             {
+ *               "group": "Static Equipment",
+ *               "items": [
+ *                 { "name": "Tank" },
+ *                 { "name": "Vessel" }
+ *               ],
+ *             }
+ *           ],
+ *         }
+ *       ]
+ *     }]
+ */
+objectsRouter.get('/types', objectController.getObjectTypes);
+
+/**
  * @api {get} /api/v1/objects/:tag Get a specific object by its identifier
  * @apiDescription Returns a resource response containing the specified object. Returns a 404 error
  * message if no such object is found.
