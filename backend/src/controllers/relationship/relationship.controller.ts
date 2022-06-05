f736471d7b6ae1cd29c0f0a9d99be3b43f582ef1
Melissa Neamt <498265@student.saxion.nl>
2022-06-05 13:22:28 +0200

    feat: add POST Relationship endpoint

--- backend/src/controllers/relationship/relationship.controller.ts
+++ backend/src/controllers/relationship/relationship.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import DI from '../../DI';
 import { TypedRequest } from '../../routes/util/typed-request';
 import { RelationshipParams } from '../../routes/relationships/relationships.types';
+import { validateItems, validateRequest, checkItemsRelationship } from './relationship.util';
+import { Relationship } from '../../database/models';
 
 export const getAllRelationships = async (
   req: Request,
@@ -27,6 +29,31 @@ export const getOneRelationship = async (
     }
 
     return res.json(relationship);
+  } catch (e: any) {
+    return res.status(400).json({
+      message: e.message,
+    });
+  }
+};
+
+export const postRelationship = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    validateRequest(req.body);
+
+    const pipeline = await DI.itemRepository.findOne(req.body.pipeline);
+    const firstItem = await DI.itemRepository.findOne(req.body.firstItem);
+    const secondItem = await DI.itemRepository.findOne(req.body.secondItem);
+
+    validateItems(pipeline, firstItem, secondItem);
+    checkItemsRelationship(firstItem, secondItem);
+
+    const relationship: Relationship = DI.em.create(Relationship, req.body);
+    await DI.relationshipRepository.persist(relationship).flush();
+
+    return res.status(201).json(relationship);
   } catch (e: any) {
     return res.status(400).json({
       message: e.message,
