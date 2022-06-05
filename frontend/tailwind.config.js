deed1d5116e524dad5aa6e5685d9a53f3c9ebff9
Quan <490848@student.saxion.nl>
2022-06-05 21:48:16 +0200

    style: custom outline offset for input fields

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      outlineOffset: {
+        m2: '-2px',
+      }
+    },
   },
   plugins: [],
 };
