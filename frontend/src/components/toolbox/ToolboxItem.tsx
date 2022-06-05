3679339ba17f1b55d8c346d5407d3e71857c903e
kristiyan2021 <449302@student.saxion.nl>
2022-06-05 19:20:58 +0200

    feat(backend): add type key to toolbox item types

--- frontend/src/components/toolbox/ToolboxItem.tsx
+++ frontend/src/components/toolbox/ToolboxItem.tsx
@@ -4,27 +4,28 @@ import { faCube } from '@fortawesome/free-solid-svg-icons';
 
 interface Item {
   className?: string;
-  name: string;
+  displayName: string;
+  type: string;
 }
 
-function ToolboxItem({ name, className = '' }: Item) {
+function ToolboxItem({ type, displayName, className = '' }: Item) {
   const onDragStart = (event: React.DragEvent, label: string) => {
     if (!event.dataTransfer) return;
-    event.dataTransfer.setData('application/reactflow', label);
+    event.dataTransfer.setData('application/reactflow', type);
     // eslint-disable-next-line no-param-reassign
     event.dataTransfer.effectAllowed = 'move';
   };
 
   return (
     <div
-      onDragStart={(event) => onDragStart(event, name)}
+      onDragStart={(event) => onDragStart(event, displayName)}
       className={`cursor-move p-1 text-sm select-none transition-all hover:bg-slate-100 hover:rounded-r-lg hover:font-bold
      ${className}`}
       draggable
-      data-cy={`toolbox-item-${name.replace(' ', '_')}`}
+      data-cy={`toolbox-item-${type}`}
     >
       <FontAwesomeIcon icon={faCube} />
-      {` ${name}`}
+      {` ${displayName}`}
     </div>
   );
 }
