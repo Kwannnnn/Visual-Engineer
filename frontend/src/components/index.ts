003f4c9e2b26c2e1b116b7f1d551af7bf87bd991
kristiyan2021 <449302@student.saxion.nl>
2022-06-07 19:08:54 +0200

    refactor: delete old unused code

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,3 @@
-export { default as Button } from './Button';
-export { default as Container } from './Container';
 export { default as Navbar } from './Navbar';
 export { default as PropertiesSidebar } from './propertiesSidebar/PropertiesSidebar';
 export { default as Board } from './board/Board';
