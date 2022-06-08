987c74c309dc2a9cb612206cb5e78cc2e9556d12
PeterP_Saxion <490498@student.saxion.nl>
2022-06-08 12:08:39 +0200

    feat: store pipeline object props in edge

--- frontend/src/util/transformConnectionToEdge.ts
+++ frontend/src/util/transformConnectionToEdge.ts
@@ -1,13 +1,19 @@
 import { Edge } from 'react-flow-renderer';
+import IObjectContext from '../typings/IObjectContext';
 import IOConnectionContext from '../typings/IOConnectionContext';
 
-export default function transformConnectionToEdge(objects: Partial<IOConnectionContext>[]) : Edge[] {
+export default function transformConnectionToEdge(allEdges: Partial<IOConnectionContext>[], allObjects: Partial<IObjectContext>[] | null) : Edge[] {
   const edges: Edge[] = [];
+  if (!allObjects) return edges;
 
-  objects.forEach((object) => {
+  allEdges.forEach((object) => {
     const {
       pipeline, firstItem, secondItem,
     } = object;
+
+    const pipelineObject = allObjects.find((item) => item.tag === pipeline);
+    if (!pipelineObject) return;
+
     if (!(pipeline && firstItem && secondItem)) return;
 
     const edge: Edge = {
@@ -19,6 +25,7 @@ export default function transformConnectionToEdge(objects: Partial<IOConnectionC
       labelStyle: { cursor: 'pointer' },
       labelBgStyle: { cursor: 'pointer' },
       style: { cursor: 'pointer', strokeWidth: 3, stroke: '#000' },
+      data: { ...pipelineObject },
     };
     edges.push(edge);
   });
