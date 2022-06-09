200ecce5ae1c856bf8c328359984e207c8b73040
kristiyan2021 <449302@student.saxion.nl>
2022-06-09 22:16:55 +0200

    fix: issue with input field outline

--- frontend/src/components/PropertiesSidebar.tsx
+++ frontend/src/components/PropertiesSidebar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { faX } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Node } from 'react-flow-renderer';
 import classNames from 'classnames';
@@ -68,64 +68,73 @@ function PropertiesSidebar(props: PropertiesSidebarProps) {
   };
 
   return (
-    <aside data-cy="properties-sidebar" className={`w-full h-full flex bg-slate-50 pt-3 pb-12 px-6 overflow-y-auto border-l border-slate-200 rounded-sm relative ${className}`}>
+    <aside data-cy="properties-sidebar" className={`w-full h-full flex bg-slate-50 overflow-y-auto border-l border-slate-200 rounded-sm relative ${className}`}>
       <form onSubmit={handleSubmit}>
-        <button
-          data-cy="close-sidebar-btn"
-          className="absolute top-3 right-3 text-gray-900 hover:bg-gray-100 rounded-lg text-sm px-3 py-2 cursor-pointer"
-          type="button"
-          onClick={onClose}
-        >
-          <FontAwesomeIcon icon={faX} size="sm" />
-        </button>
-
-        <div>
-          <h2 data-cy="siderbar-item-type" className="text-xl inline-block font-semibold mb-4">{currentNode?.data.type ?? ''}</h2>
-          { currentNode && currentNode.data.isDraft && (
-            <span className="bg-amber-100 rounded-md text-amber-600 uppercase px-1.5 py-1.5 ml-1.5 text-sm font-medium">Draft</span>
+        <div className="sticky top-0 px-6 py-3 bg-white shadow-md">
+          <div className="flex items-center justify-between">
+            <span>
+              <h2 data-cy="siderbar-item-type" className="text-xl inline-block cursor-default font-semibold">{currentNode?.data.type ?? ''}</h2>
+              { currentNode && currentNode.data.isDraft && (
+                <span className="bg-amber-100 rounded-md text-amber-600 uppercase px-1.5 py-1.5 ml-1.5 text-sm font-medium">Draft</span>
+              )}
+            </span>
+            <FontAwesomeIcon
+              data-cy="close-sidebar-btn"
+              icon={faXmark}
+              className="hover:bg-gray-100 cursor-pointer text-gray-500 hover:text-gray-800 rounded-full px-2 py-1.5"
+              size="sm"
+              onClick={onClose}
+            />
+          </div>
+          { currentNode && !currentNode.data.isDraft && (
+            <input
+              type="text"
+              value={currentNode?.data.tag}
+              className="rounded-lg w-full mt-2 mb-3 border border-gray-300 bg-zinc-200 text-slate-400 px-3 py-2 cursor-not-allowed"
+              disabled
+            />
           )}
-        </div>
 
-        <div className="flex space-x-2 w-full mb-6">
-          { currentNode && currentNode.data.isDraft && (
+          <div className={classNames('flex space-x-2 w-full mb-4', {
+            'mt-4': currentNode && currentNode.data.isDraft,
+          })}
+          >
+            { currentNode && currentNode.data.isDraft && (
             <button id="save-component-btn" type="submit" className="rounded-lg w-1/2 p-2 shadow-sm hover:shadow-md border border-green-700 hover:bg-green-700 text-green-700 hover:text-white py-2 cursor-pointer mt-auto">
               <p className="hidden md:inline"> Publish</p>
             </button>
-          )}
-          <button
-            id="delete-component-btn"
-            className={classNames('rounded-lg p-2 shadow-sm hover:shadow-md border border-red-700 hover:bg-red-700 text-red-700 hover:text-white py-2 cursor-pointer', {
-              'w-1/2': currentNode && currentNode.data.isDraft,
-              'w-full': !(currentNode && currentNode.data.isDraft),
-            })}
-            type="button"
-          >
-            <p className="hidden md:inline"> Delete</p>
-          </button>
+            )}
+            <button
+              id="delete-component-btn"
+              className={classNames('rounded-lg p-2 shadow-sm hover:shadow-md border border-red-700 hover:bg-red-700 text-red-700 hover:text-white py-2 cursor-pointer', {
+                'w-1/2': currentNode && currentNode.data.isDraft,
+                'w-full': !(currentNode && currentNode.data.isDraft),
+              })}
+              type="button"
+            >
+              <p className="hidden md:inline"> Delete</p>
+            </button>
+          </div>
         </div>
 
-        <hr />
-
-        <div data-cy="sidebar-properties-list" className="w-full">
+        <div data-cy="sidebar-properties-list" className="flex flex-col w-full space-y-4 px-6 pt-6 pb-12">
           {
             propValues.map((p) => {
               const value = getPropertyValue(currentNode, p.name);
               p.value = value;
 
               return (
-                <div className="flex flex-col overflow-y-auto mt-3" key={p.name}>
-                  <label htmlFor={`sidebar-input-field-${p.name}`}>
-                    {p.name}
-                    <input
-                      name={p.name}
-                      type={p.type}
-                      data-cy={`sidebar-input-field-${p.name}`}
-                      value={p.value}
-                      onChange={(event) => handleChange(event)}
-                      className="focus:outline-sky-600 rounded-lg w-full bg-gray-200 text-slate-700 px-3 py-2 mt-1"
-                    />
-                  </label>
-                </div>
+                <label htmlFor={`sidebar-input-field-${p.name}`}>
+                  {p.name}
+                  <input
+                    name={p.name}
+                    type={p.type}
+                    data-cy={`sidebar-input-field-${p.name}`}
+                    value={p.value}
+                    onChange={(event) => handleChange(event)}
+                    className="focus:ring-2 focus:ring-wb-blue/60 outline-none border border-gray-300 rounded-lg w-full bg-gray-200 text-slate-700 px-3 py-2 mt-1"
+                  />
+                </label>
               );
             })
           }
