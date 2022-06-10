007b88b5d46338207697ea1fd763f57b951cced5
PeterP_Saxion <490498@student.saxion.nl>
2022-06-10 17:41:29 +0200

    test(cypress): add edge placeholder test

--- frontend/cypress/integration/PropertiesSidebar.spec.js
+++ frontend/cypress/integration/PropertiesSidebar.spec.js
@@ -1,93 +1,164 @@
 beforeEach(() => {
-	cy.visit('http://localhost:8080');
-	cy.viewport(1440, 900);
+  cy.visit('http://localhost:8080');
+  cy.viewport(1440, 900);
 });
 
 const getBoard = () => cy.get('[data-cy=board]')
-	.first() // react-flow
-	.first() // react-flow__renderer
-	.first() // react-flow__viewport
-	.first() // react-flow__edges
-	.first() // react-flow__nodes
-	.as('board');
+  .first() // react-flow
+  .first() // react-flow__renderer
+  .first() // react-flow__viewport
+  .first() // react-flow__edges
+  .first() // react-flow__nodes
+  .as('board');
 
 const getPropertiesSidebar = () => cy.get('[data-cy=properties-sidebar]').as('sidebar');
 const getPropertiesSidebarList = () => cy.get('[data-cy=sidebar-properties-list]').as('sidebar-prop-list');
 
 const getVessel = () => cy.get('[data-cy=toolbox-item-vessel]')
-	.as('vessel');
+  .as('vessel');
 
 const getPipeFitting = () => cy.get('[data-cy=toolbox-item-pipeFitting]')
-	.as('pipeFitting');
+  .as('pipeFitting');
+
+const closeSidebar = () => {
+  cy.get('@sidebar')
+    .should('exist')
+    .should('be.visible');
+  cy.get('[data-cy=close-sidebar-btn]')
+    .should('exist')
+    .should('be.visible')
+    .click();
+  cy.get('@sidebar')
+    .should('exist')
+    .should('not.be.visible');
+};
 
 const dragAndDropVessel = () => {
-	const dataTransfer = new DataTransfer();
+  const dataTransfer = new DataTransfer();
 
-	getVessel();
-	getBoard();
-	getPropertiesSidebar();
-	getPropertiesSidebarList();
+  getVessel();
+  getBoard();
+  getPropertiesSidebar();
+  getPropertiesSidebarList();
 
-	cy.get('@sidebar')
-		.should('exist')
-		.should('not.be.visible')
+  cy.get('@sidebar')
+    .should('exist')
+    .should('not.be.visible');
 
-	cy.get('@board')
-		.should('exist')
-		.should('be.visible');
+  cy.get('@board')
+    .should('exist')
+    .should('be.visible');
 
-	cy.get('@vessel')
-		.should('exist')
-		.should('be.visible');
+  cy.get('@vessel')
+    .should('exist')
+    .should('be.visible');
 
-	cy.get('@vessel')
-		.click()
-		.trigger('dragstart', { dataTransfer });
+  cy.get('@vessel')
+    .click()
+    .trigger('dragstart', { dataTransfer });
 
-	cy.get('@board')
-		.trigger('drop', { dataTransfer });
-}
+  cy.get('@board')
+    .trigger('drop', { dataTransfer });
+};
+
+const dragAndDropPipeFitting = () => {
+  const dataTransfer = new DataTransfer();
+
+  getPipeFitting();
+  getBoard();
+  getPropertiesSidebar();
+  getPropertiesSidebarList();
+
+  cy.get('@sidebar')
+    .should('exist')
+    .should('not.be.visible');
+
+  cy.get('@board')
+    .should('exist')
+    .should('be.visible');
+
+  cy.get('@pipeFitting')
+    .should('exist')
+    .should('be.visible');
+
+  cy.get('@pipeFitting')
+    .click()
+    .trigger('dragstart', { dataTransfer });
+
+  cy.get('@board')
+    .trigger('drop', { dataTransfer });
+};
 
 describe('PropertiesSidebar', () => {
-	describe('When "Vessel" is dragged and dropped from the Toolbox to the Board', () => {
-		it('should populate the PropertiesSidebar with empty input fields', () => {
-			dragAndDropVessel();
+  describe('When "Vessel" is dragged and dropped from the Toolbox to the Board', () => {
+    it('should populate the PropertiesSidebar with empty input fields', () => {
+      dragAndDropVessel();
 
-			cy.get('@sidebar')
-			.should('exist')
-			.should('be.visible');
+      cy.get('@sidebar')
+        .should('exist')
+        .should('be.visible');
 
-			cy.get('@sidebar-prop-list')
-				.children()
-				.should('exist')
-				.should('be.visible');
+      cy.get('@sidebar-prop-list')
+        .children()
+        .should('exist')
+        .should('be.visible');
 
-			cy.get('@sidebar-prop-list')
-				.children().each(($el) => {
-					cy.wrap($el)
-						.first()
-						.first()
-						.should('have.value', '');
-				});
-		});
+      cy.get('@sidebar-prop-list')
+        .children().each(($el) => {
+          cy.wrap($el)
+            .first()
+            .first()
+            .should('have.value', '');
+        });
+    });
 
-		describe('When the close button is clicked on the PropertiesSidebar', () => {
-			it('should close the PropertiesSidebar', () => {
-				dragAndDropVessel();
+    describe('When the close button is clicked on the PropertiesSidebar', () => {
+      it('should close the PropertiesSidebar', () => {
+        dragAndDropVessel();
+        closeSidebar();
+      });
+    });
 
-				cy.get('@sidebar')
-				.should('exist')
-				.should('be.visible');
+    /*
+    describe('When pipeline is clicked', () => {
+      it('Should show property list for the draft pipeline', () => {
+        const dataTransfer = new DataTransfer();
+        // Render 2 connectable nodes on the board
+        dragAndDropVessel();
+        closeSidebar();
+        dragAndDropPipeFitting();
+        closeSidebar();
 
-				cy.get('[data-cy=close-sidebar-btn]')
-				.should('exist')
-				.should('be.visible')
-				.click();
+        // Connect the 2 nodes
+        cy.get('@vessel')
+          .should('exist')
+          .should('be.visible');
+        cy.get('@pipeFitting')
+          .should('exist')
+          .should('be.visible');
+        cy.get('@board')
+          .get('[data-cy=right-itemNode_1]')
+          .should('exist')
+          .trigger('dragstart', { dataTransfer, force: true });
+        cy.get('@board')
+          .get('[data-cy=left-itemNode_3]')
+          .should('exist')
+          .trigger('drop', { dataTransfer, force: true });
 
-				cy.get('@sidebar')
-				.should('exist')
-				.should('not.be.visible');
-			});
-		});
-	});
+        // Check if the prop sidebar is visible
+        cy.get('@sidebar')
+          .should('exist')
+          .should('be.visible');
+        cy.get('@sidebar-prop-list')
+          .children()
+          .should('exist')
+          .should('be.visible');
+
+        // Check if the edge is visible
+        cy.get('.itemTmpEdge_0')
+          .should('exist')
+          .should('be.visible');
+      });
+    }); */
+  });
 });
