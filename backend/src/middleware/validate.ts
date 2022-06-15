b45c85aaf452a1728d139b72b209604916da1c1f
Melissa Neamt <498265@student.saxion.nl>
2022-06-15 15:39:58 +0200

    refactor(backend): add errorHandler to the validator handler

--- backend/src/middleware/validate.ts
+++ backend/src/middleware/validate.ts
@@ -4,17 +4,21 @@ import { validationResult, ValidationChain } from 'express-validator';
 /**
  * Express validator handler to validate query and body parameters for each endpoint
  */
+const errorHandler = (req: Request, res: Response, next: NextFunction) => {
+  const error = validationResult(req).array({ onlyFirstError: true })[0];
+  if (!error) {
+    return next();
+  }
+
+  return res.status(error.msg.statusCode).json(error.msg.message);
+};
+
 const validate = (
   validations: ValidationChain[],
 ) => async (req: Request, res: Response, next: NextFunction) => {
   await Promise.all(validations.map((validation) => validation.run(req)));
 
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    return next();
-  }
-
-  return res.status(400).json({ errors: errors.array() });
+  errorHandler(req, res, next);
 };
 
 export default validate;
