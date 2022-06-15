b1984954fb890bb19338358c4f8fc5ae25c93977
kristiyan2021 <449302@student.saxion.nl>
2022-06-15 17:44:40 +0200

    fix: retouch tabBar styling to prevent double clicking

--- frontend/src/components/board/tabbar/TabBar.tsx
+++ frontend/src/components/board/tabbar/TabBar.tsx
@@ -6,23 +6,33 @@ interface TabBarProps {
   currentBoardId: number;
   boards: IBoard[];
   onSelect?: (id: number) => void;
+  onClose?: (id: number) => void;
 }
 
-function TabBar({ currentBoardId, boards, onSelect }: TabBarProps) {
+function TabBar({
+  currentBoardId, boards, onSelect, onClose,
+}: TabBarProps) {
   const handleSelect = (id: number) => {
     if (onSelect) {
       onSelect(id);
     }
   };
 
+  const handleClose = (id: number) => {
+    if (onClose) {
+      onClose(id);
+    }
+  };
+
   return (
-    <div className="bg-white border-b border-slate-200 z-10 flex">
+    <div className="bg-white border-b border-slate-200 z-10 w-full h-12 flex">
       {boards.map((board) => (
         <Tab
           key={board.id}
           id={board.id}
           name={board.name}
           onSelect={handleSelect}
+          onClose={handleClose}
           active={board.id === currentBoardId}
         />
       ))}
