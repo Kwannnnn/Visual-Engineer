51c395198d8cde467a42de716fc7282a738fb1f4
PeterP_Saxion <490498@student.saxion.nl>
2022-06-15 14:21:16 +0200

    feat: have only one source and target handle on node
    Co-authored-by: Philippos Slicher <philipposslicher@gmail.com>

--- frontend/src/components/board/ItemNode.tsx
+++ frontend/src/components/board/ItemNode.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useContext } from 'react';
 import { Handle, Position } from 'react-flow-renderer';
+import { DragContext } from './Board';
 
 interface ItemNodeProps {
   className?: string;
@@ -18,6 +19,8 @@ function ItemNode(props: ItemNodeProps) {
     tag = '', type, dataCY, isDraft,
   } = data;
 
+  const dragContext = useContext(DragContext);
+
   return (
     <>
       <Handle
@@ -25,58 +28,51 @@ function ItemNode(props: ItemNodeProps) {
         position={Position.Top}
         id={`top-${dataCY}`}
         data-cy={`top-${dataCY}`}
-        className="hover:border-4 hover:border-gray-300"
-        style={{
-          height: 14,
-          width: 14,
-          top: -7,
-        }}
-      />
-      <Handle
-        type="target"
-        position={Position.Left}
-        id={`left-${dataCY}`}
-        data-cy={`left-${dataCY}`}
-        className="hover:border-4 hover:border-gray-300"
-        style={{
-          height: 14,
-          width: 14,
-          left: -7,
-        }}
+        className="hover:border-4 hover:border-gray-300 hidden"
+        // style={{
+        //   height: 14,
+        //   width: 14,
+        //   top: -7,
+        // }}
       />
       <div
         data-cy={dataCY}
         className={classNames(`bg-slate-50 border-2 px-12 py-8 border-black ${className}`, {
           'bg-amber-50': isDraft,
         })}
+        style={{ zIndex: 5 }}
       >
         <div>{tag}</div>
         <div>{type}</div>
+        <Handle
+          type="source"
+          position={Position.Top}
+          id={`bottom-${dataCY}`}
+          data-cy={`bottom-${dataCY}`}
+          className="hover:border-4 hover:border-gray-300"
+          style={{
+            height: 14,
+            width: 14,
+            top: 20,
+          }}
+        />
+        <Handle
+          type="target"
+          position={Position.Bottom}
+          id={`bottom-${dataCY}`}
+          data-cy={`bottom-${dataCY}`}
+          className="hover:border-4 hover:border-gray-300"
+          style={{
+            height: '100%',
+            width: '100%',
+            bottom: 0,
+            borderRadius: 0,
+            display: !dragContext.connecting ? 'none' : 'initial',
+            backgroundColor: 'transparent',
+            border: 'none',
+          }}
+        />
       </div>
-      <Handle
-        type="source"
-        position={Position.Bottom}
-        id={`bottom-${dataCY}`}
-        data-cy={`bottom-${dataCY}`}
-        className="hover:border-4 hover:border-gray-300"
-        style={{
-          height: 14,
-          width: 14,
-          bottom: -7,
-        }}
-      />
-      <Handle
-        type="source"
-        position={Position.Right}
-        id={`right-${dataCY}`}
-        data-cy={`right-${dataCY}`}
-        className="hover:border-4 hover:border-gray-300"
-        style={{
-          height: 14,
-          width: 14,
-          right: -7,
-        }}
-      />
     </>
   );
 }
