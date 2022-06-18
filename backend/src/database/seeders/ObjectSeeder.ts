5b070afcba0ac3b5d4883f1553579244cb3cf949
kristiyan2021 <449302@student.saxion.nl>
2022-06-18 11:35:09 +0200

    fix: separate seeded data cluster further apart

--- backend/src/database/seeders/ObjectSeeder.ts
+++ backend/src/database/seeders/ObjectSeeder.ts
@@ -17,12 +17,37 @@ export default class ObjectSeeder extends Seeder {
 
     // Add sample items to the exported array
     sampleBoardObjects.push(new PipelineFactory(em).makeOne({ board: sampleBoardId, tag: 'PL01' }));
-    sampleBoardObjects.push(new PumpFactory(em).makeOne({ board: sampleBoardId, tag: 'PU01' }));
-    sampleBoardObjects.push(new BlowerFactory(em).makeOne({ board: sampleBoardId, tag: 'BL01' }));
-    sampleBoardObjects.push(new PipeFittingFactory(em).makeOne({ board: sampleBoardId, tag: 'PF01' }));
+    sampleBoardObjects.push(new PumpFactory(em).makeOne({
+      board: sampleBoardId,
+      tag: 'PU01',
+      x: 50,
+      y: 50,
+    }));
+    sampleBoardObjects.push(new BlowerFactory(em).makeOne({
+      board: sampleBoardId,
+      tag: 'BL01',
+      x: 250,
+      y: 125,
+    }));
+    sampleBoardObjects.push(new PipeFittingFactory(em).makeOne({
+      board: sampleBoardId,
+      tag: 'PF01',
+      x: 250,
+      y: 500,
+    }));
     sampleBoardObjects.push(new PipelineFactory(em).makeOne({ board: sampleBoardId, tag: 'PL02' }));
-    sampleBoardObjects.push(new TankFactory(em).makeOne({ board: sampleBoardId, tag: 'T01' }));
-    sampleBoardObjects.push(new PipeFittingFactory(em).makeOne({ board: sampleBoardId, tag: 'PF02' }));
+    sampleBoardObjects.push(new TankFactory(em).makeOne({
+      board: sampleBoardId,
+      tag: 'T01',
+      x: 610,
+      y: 200,
+    }));
+    sampleBoardObjects.push(new PipeFittingFactory(em).makeOne({
+      board: sampleBoardId,
+      tag: 'PF02',
+      x: 900,
+      y: 200,
+    }));
 
     // Assign the newly created items to the sample board
     sampleBoardObjects.forEach((object) => sampleBoardItems.add(object));
