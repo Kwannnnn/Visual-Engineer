17c92be8f8da0211392806c637d5e207b218d290
Melissa Neamt <498265@student.saxion.nl>
2022-06-19 13:30:57 +0200

    feat: add validators for POST object to Board

--- backend/src/middleware/boards.validators.ts
+++ backend/src/middleware/boards.validators.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { body } from 'express-validator';
+import DI from '../DI';
 import ValidationError from '../error/ValidationError';
 import validate from './validate';
 
@@ -9,7 +10,33 @@ const validators = () => [
     .withMessage(new ValidationError('Board name is missing', 400)),
 ];
 
-const boardValidators = (
+export const postObjectToBoardValidators = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const validatorsArray = validators();
+  validatorsArray.push(
+    body()
+      .custom(() => {
+        const id = +req.params.id;
+        return DI
+          .boardRepository
+          .findOne({ id })
+          .then((board) => {
+            if (!board) {
+              return Promise.reject();
+            }
+            return true;
+          });
+      })
+      .withMessage(new ValidationError('Board not found', 404)),
+  );
+
+  validate(validatorsArray)(req, res, next);
+};
+
+export const postBoardValidators = (
   req: Request,
   res: Response,
   next: NextFunction,
@@ -17,5 +44,3 @@ const boardValidators = (
   const validatorsArray = validators();
   validate(validatorsArray)(req, res, next);
 };
-
-export default boardValidators;
